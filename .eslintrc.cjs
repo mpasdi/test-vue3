f8e1a4822f25554d4dfeca5373cbbd84c3007168
@medo_lsm <songming.li@shmedo.cn>
2023-12-25 17:59:12 +0800

    fix: 修改 package type-check命令

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,9 @@
+/*
+ * @Author: lisongming
+ * @Date: 2023-12-21 16:25:10
+ * @LastEditors: lisongming
+ * @Description: 描述
+ */
 /* eslint-env node */
 require('@rushstack/eslint-patch/modern-module-resolution')
 
