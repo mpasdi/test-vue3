94125c98bb35c432ef759a9870b308e91e1e1c42
lsm <2779355293@qq.com>
2023-12-30 17:55:53 +0800

    test: rangePicker时间控件测试

--- src/views/background/modules/ComponentsTest.vue
+++ src/views/background/modules/ComponentsTest.vue
@@ -17,14 +17,25 @@
     />
 
     <h2>2. antd rangePicker 测试</h2>
-    <a-range-picker
-      showTime
-      :value="hackValue || value"
-      :disabled-date="disabledDate"
-      @change="onChange"
-      @openChange="onOpenChange"
-      @calendarChange="onCalendarChange"
-    />
+    <div>
+      <a-range-picker
+        showTime
+        :value="hackValue || value"
+        :disabled-date="disabledDate"
+        @change="onChange"
+        @openChange="onOpenChange"
+        @calendarChange="onCalendarChange"
+        class="disabled-css"
+      />
+    </div>
+
+    <!--    3. antd rangePicker v-model测试 -->
+    <h2>3. antd rangePicker v-model测试</h2>
+    <a-range-picker showTime v-model:value="commonTimevalue" />
+    选中时间 time： {{ commonTimevalue }}
+    <br />
+    <a-range-picker v-model:value="commonTimevalue" />
+    选中时间： {{ commonTimevalue }}
   </div>
 </template>
 
@@ -36,9 +47,19 @@
   type RangeValue = [Dayjs, Dayjs]
 
   // 2. antd rangePicker 测试
+  /**
+   *  解决方式：
+   *    1. 选择完时间 进行验证
+   *    2. 切换 输入框 进行验证
+   *    3. 进制切换
+   *    4. 点击picker的时候触发 按钮点击事件
+   *    5. 在弹窗的时候禁止
+   */
   const dates = ref<RangeValue>()
   const value = ref<RangeValue>()
   const hackValue = ref<RangeValue>()
+
+  const disabledcss = ref('all')
 
   const disabledDate = (current: Dayjs) => {
     if (!dates.value || (dates.value as any).length === 0) {
@@ -53,8 +74,10 @@
     if (open) {
       dates.value = [] as any
       hackValue.value = [] as any
+      disabledcss.value = 'none'
     } else {
       hackValue.value = undefined
+      disabledcss.value = 'all'
     }
   }
 
@@ -65,10 +88,17 @@
   const onCalendarChange = (val: RangeValue) => {
     dates.value = val
   }
+
+  //   3. antd rangePicker v-model测试
+  const commonTimevalue = ref<RangeValue>()
 </script>
 
 <style scoped lang="less">
   .input-image {
     width: 200px;
+  }
+
+  .disabled-css {
+    pointer-events: v-bind(disabledcss);
   }
 </style>
