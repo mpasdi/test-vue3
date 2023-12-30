8eb60edf4a8762fdf7bfafd77ff698fd111d96e2
lsm <2779355293@qq.com>
2023-12-30 14:41:43 +0800

    test: 测试rangePicker控件时间范围选择问题

--- src/utils/registerUiComponent.ts
+++ src/utils/registerUiComponent.ts
@@ -1,6 +1,6 @@
 import { App } from 'vue'
-import { Button, Input } from 'ant-design-vue'
+import { Button, Input, DatePicker } from 'ant-design-vue'
 
 export function registerUiComponents(app: App) {
-  app.use(Button).use(Input)
+  app.use(Button).use(Input).use(DatePicker)
 }
--- src/views/background/modules/ComponentsTest.vue
+++ src/views/background/modules/ComponentsTest.vue
@@ -15,10 +15,57 @@
       src="http://122.152.220.184:9000/bucket-1/image/js_1702216443238.webp"
       class="input-image"
     />
+
+    <h2>2. antd rangePicker 测试</h2>
+    <a-range-picker
+      showTime
+      :value="hackValue || value"
+      :disabled-date="disabledDate"
+      @change="onChange"
+      @openChange="onOpenChange"
+      @calendarChange="onCalendarChange"
+    />
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import { ref } from 'vue'
+  import { Dayjs } from 'dayjs'
+
+  //types
+  type RangeValue = [Dayjs, Dayjs]
+
+  // 2. antd rangePicker 测试
+  const dates = ref<RangeValue>()
+  const value = ref<RangeValue>()
+  const hackValue = ref<RangeValue>()
+
+  const disabledDate = (current: Dayjs) => {
+    if (!dates.value || (dates.value as any).length === 0) {
+      return false
+    }
+    const tooLate = dates.value[0] && current.diff(dates.value[0], 'days') > 7
+    const tooEarly = dates.value[1] && dates.value[1].diff(current, 'days') > 7
+    return tooEarly || tooLate
+  }
+
+  const onOpenChange = (open: boolean) => {
+    if (open) {
+      dates.value = [] as any
+      hackValue.value = [] as any
+    } else {
+      hackValue.value = undefined
+    }
+  }
+
+  const onChange = (val: RangeValue) => {
+    value.value = val
+  }
+
+  const onCalendarChange = (val: RangeValue) => {
+    dates.value = val
+  }
+</script>
 
 <style scoped lang="less">
   .input-image {
