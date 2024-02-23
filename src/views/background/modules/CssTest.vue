9355e6bf0d28b2df5fcd26c6d0be2a18fa10872e
@medo_lsm <songming.li@shmedo.cn>
2024-02-23 14:14:24 +0800

    feat: 新增css 测试

--- src/views/background/modules/CssTest.vue
+++ src/views/background/modules/CssTest.vue
@@ -7,9 +7,32 @@
 <template>
   <div>
     <h1>css 样式测试</h1>
+    <h2>nth-type-of & nth-child 测试</h2>
+    <div class="nth">
+      <span v-for="item in 4" :key="item">{{ item }}</span>
+    </div>
   </div>
 </template>
 
 <script setup></script>
 
-<style lang="scss" scoped></style>
+<style lang="less" scoped>
+  .nth {
+    span {
+      display: inline-block;
+      width: 20px;
+      height: 20px;
+      background: gray;
+      margin-right: 10px;
+      color: white;
+    }
+
+    span:nth-child(2) {
+      background-color: rebeccapurple;
+    }
+
+    span:nth-of-type(3) {
+      background-color: palevioletred;
+    }
+  }
+</style>
