430233c396e5208a63e7d6dd14a805bbd25892c8
@medo_lsm <songming.li@shmedo.cn>
2024-02-27 09:36:06 +0800

    feat: js复习

--- src/router/modules/review.ts
+++ src/router/modules/review.ts
@@ -1,3 +1,9 @@
+/*
+ * @Author: lisongming
+ * @Date: 2024-01-15 13:12:49
+ * @LastEditors: lisongming
+ * @Description: 描述
+ */
 import { RouteRecordRaw } from 'vue-router'
 
 export const Review: RouteRecordRaw[] = [
@@ -12,10 +18,18 @@ export const Review: RouteRecordRaw[] = [
     children: [
       {
         path: 'tsTest',
-        name: '/tsTest',
+        name: 'TsTest',
         component: () => import('@/views/review/modules/TsTest.vue'),
         meta: {
           title: 'ts复习'
+        }
+      },
+      {
+        path: 'jsPath',
+        name: 'JsPath',
+        component: () => import('@/views/review/modules/JsReview.vue'),
+        meta: {
+          title: 'js复习'
         }
       }
     ]
---
+++ src/views/review/modules/JsReview.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>js 复习</h1>
+
+    <h2>filter(Boolean)</h2>
+    <div>old arr: {{ arr }}</div>
+    <div>filter arr: {{ filterArr }}</div>
+
+    <h2>空数组 的some 和 every 方法</h2>
+    <div>some: {{ [].some(() => {}) }}</div>
+    <div>every: {{ [].every(() => {}) }}</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { computed } from 'vue'
+
+  const arr = [1, '31', null, NaN, true, false, undefined, Infinity, 0]
+  const filterArr = computed(() => {
+    return arr.filter(Boolean)
+  })
+</script>
+
+<style scoped lang="less"></style>
