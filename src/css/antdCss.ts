07864031be023c382653d494b6a061afe8632dd5
@medo_lsm <songming.li@shmedo.cn>
2024-02-28 16:50:07 +0800

    feat: antd 组件样式按需加载

---
+++ src/css/antdCss.ts
@@ -0,0 +1,7 @@
+import 'ant-design-vue/es/button/style/css'
+import 'ant-design-vue/es/table/style/css'
+import 'ant-design-vue/es/input/style/css'
+import 'ant-design-vue/es/date-picker/style/css'
+import 'ant-design-vue/es/tabs/style/css'
+import 'ant-design-vue/es/carousel/style/css'
+import 'ant-design-vue/es/table/style/css'
