53529873dd35110e4b1e4c78fdef30772c6af811
@medo_lsm <songming.li@shmedo.cn>
2024-02-29 10:24:00 +0800

    refactor: 修改按需导入实现方案

--- src/css/antdCss.ts
+++ src/css/antdCss.ts
@@ -1,7 +1,13 @@
-import 'ant-design-vue/es/button/style/css'
-import 'ant-design-vue/es/table/style/css'
-import 'ant-design-vue/es/input/style/css'
-import 'ant-design-vue/es/date-picker/style/css'
-import 'ant-design-vue/es/tabs/style/css'
-import 'ant-design-vue/es/carousel/style/css'
-import 'ant-design-vue/es/table/style/css'
+/*
+ * @Author: lisongming
+ * @Date: 2024-02-28 16:19:52
+ * @LastEditors: lisongming
+ * @Description: 描述
+ */
+// import 'ant-design-vue/es/button/style/css'
+// import 'ant-design-vue/es/table/style/css'
+// import 'ant-design-vue/es/input/style/css'
+// import 'ant-design-vue/es/date-picker/style/css'
+// import 'ant-design-vue/es/tabs/style/css'
+// import 'ant-design-vue/es/carousel/style/css'
+// import 'ant-design-vue/es/table/style/css'
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,26 @@
+/*
+ * @Author: lisongming
+ * @Date: 2024-02-27 09:38:37
+ * @LastEditors: lisongming
+ * @Description: 描述
+ */
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import Components from 'unplugin-vue-components/vite'
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    Components({
+      resolvers: [AntDesignVueResolver()]
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
