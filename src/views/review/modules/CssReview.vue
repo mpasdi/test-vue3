c6f69c8e9156bc4fd45f68a00a7f02a70e059da9
@medo_lsm <songming.li@shmedo.cn>
2024-03-08 16:54:21 +0800

    feat：添加grid布局测试

--- src/views/review/modules/CssReview.vue
+++ src/views/review/modules/CssReview.vue
@@ -11,6 +11,14 @@
     <div class="css-flex">
       <div class="css-flex_item" v-for="item in 4" :key="item">{{ item }}</div>
     </div>
+
+    <h2>gird 布局</h2>
+    <div class="css-grid">
+      <div class="css-grid_item" v-for="item in 7" :key="item">{{ item }}</div>
+    </div>
+    <div class="css-grid-area">
+      <div class="css-grid-area_item" v-for="item in 4" :key="item">{{ item }}</div>
+    </div>
   </div>
 </template>
 
@@ -38,6 +46,53 @@
     }
     &_item:nth-child(4) {
       order: 0;
+    }
+  }
+
+  .css-grid {
+    background-color: orange;
+    font-size: 20px;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr); // 容器：垂直三列
+    // grid-template-columns: 1fr 2fr 1fr;
+    // grid-template-rows: 2fr 1fr;
+    row-gap: 20px;
+    gap: 10px 10px; // 间隙， row column
+
+    &_item {
+      // width: 30px;
+      border: 2px solid saddlebrown;
+    }
+    &_item:nth-child(1) {
+      color: white;
+      grid-column: span 2;
+    }
+    &_item:nth-child(4) {
+      color: white;
+      grid-row: span 2;
+    }
+  }
+
+  .css-grid-area {
+    background-color: skyblue;
+    font-size: 20px;
+    display: grid;
+    grid-template-areas: 'header header' 'header body' 'foot .';
+    margin-top: 20px;
+    grid-gap: 10px 10px;
+    justify-items: center; // 容器项居中
+    height: 200px;
+    // justify-content: center; // 容器居中
+    align-items: center;
+    align-content: center;
+    &_item {
+      // width: 30px;
+      height: 50px;
+      border: 2px solid saddlebrown;
+    }
+
+    &_item:nth-child(1) {
+      grid-area: header;
     }
   }
 </style>
