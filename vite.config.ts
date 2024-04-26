96ab9467438bb10accef3b29146905ac3b1bcecd
lsm <2779355293@qq.com>
2024-04-26 18:21:52 +0800

    fix: 🧩 修改vite.config.ts 解决生产报错

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/test-vue3/',
   build: {
     target: 'esnext'
   },
